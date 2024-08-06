4052c2308aa1915700133a29d4261ef7e2498d6a
GennaBari <barinovaevgenia84@gmail.com>
2024-08-06 19:23:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
+def calculate_structure_sum(*args):
+    summ = 0
+    for i in args:
+        if isinstance(i, int):
+            summ += i
+        elif isinstance(i, str):
+            summ += len(i)
+        elif isinstance(i, dict):
+            summ += calculate_structure_sum(*i.items())
+        elif isinstance(i, (list, tuple, set)):
+            summ += calculate_structure_sum(*i)
+    return summ
+
+
+
 data_structure = [
 [1, 2, 3],
 {'a': 4, 'b': 5},
@@ -8,3 +23,4 @@ data_structure = [
 
 result = calculate_structure_sum(data_structure)
 print(result)
+
